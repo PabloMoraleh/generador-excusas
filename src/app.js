00636a967ae6db02b5a35bbf490935daba06865b
PabloMoraleh <117026954+PabloMoraleh@users.noreply.github.com>
2024-02-12 10:35:17 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/windows.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#la-excusa").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let who = ["El perro", "Mi abuela", "Mi tortuga", "Mi pajaro"];
+  let action = ["se comió", "orinó", "aplastó", "rompió"];
+  let what = ["mi tarea", "las llaves", "el coche"];
+  let when = [
+    "antes de clases",
+    "justo a tiempo",
+    "cuando acabé",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
